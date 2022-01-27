f574278626978aec75cd7d9e4f47b74c5c1bd4a1
Zaida Kodzoeva <zkod777@gmail.com>
2022-01-27 15:36:42 +0400

    Login page fixed. And more CSS edits.

--- web/news-list.js
+++ web/news-list.js
@@ -15,7 +15,7 @@ console.log(newsList);
     newsJson.slice(0, 5).forEach(article => {
         console.log(article);
         newsList.innerHTML += `
-            <li style="display:flex;">
+            <li style="display:flex;" class="news-list-el">
                 <img src="${article.imageUrl}"/>
                 <div>
                 <a href="${article.url}"><h4>${article.title}</h4></a>
