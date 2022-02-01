4eb35fcf5f064c75991aa1ae49ec90cb46137e7c
Zaida Kodzoeva <zkod777@gmail.com>
2022-02-01 23:28:53 +0400

    Changed Home Page welcome message

--- web/home.js
+++ web/home.js
@@ -60,7 +60,7 @@ function updateHome() {
   var hi = document.getElementById("hi");
   var userName = sessionStorage.getItem("userName"); //localStorage instead of session will keep it open across tabs
   if (userName) {
-    hi.innerText = `Hi ${userName}!!!`;
+    hi.innerText = `Hi ${userName}, welcome Home!`;
 
     var myMangaList = document.getElementById("myMangaList");
     var myAnimeList = document.getElementById("myAnimeList");
